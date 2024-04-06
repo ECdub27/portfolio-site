b6ce362a2d940dbb43c59552b5bb938bb3154480
Elijah-Christian Wiegand <ewiegand51@gmail.com>
2024-04-06 08:54:04 -0500

    deleted api folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store/indexStore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <Provider store={store}>
+  
     <App  />,
-    </Provider>,
+   
   document.getElementById('root')
 );
 
