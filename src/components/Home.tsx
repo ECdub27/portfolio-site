47b3b0fda63414b4e6ed96a0d8615f1f655ca033
EC Wiegand <ewiegand51@gmail.com>
2024-08-19 20:37:01 -0500

    add some like clean hovers

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,34 +5,33 @@ import groupChatGen from '../../public/imgs/GroupChatNameGen.png';
 import Liverpool from '../../public/imgs/Liverpoolshield.jpg';
 import ProjectCard from './projectCard';  
 import ContactForm from './contactForm';
-import ThreeScene from './ThreeScene';
-
+import Typical from 'react-typical';  
 // palette colors 1AB7FF -blue bright chartruese - 2CF000 202827
 const Home = () => (
-  <div className="Home">
+  <div className="Home  bg-[#f0f4f8]">
   
   {/* nav bar section uses the introduce my name with Elijah-Christian "EC" Wiegand  #212121'*/}
-<nav className='fixed start-0 top-0 z-20 hidden w-full border-b border-[#212121]-200 bg-[#1AB7FF] sm:block'>
+<nav className='fixed start-0 top-0 z-20 hidden w-full border-b border-[#212121]-200 bg-[#267FF3] sm:block'>
   <div className='mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4'>
  {/* logo here */}
  <button data-collapse-toggle="navbar-default"
 type="button"
-className='inline-flex- h-10 w-10 items-center bg-[#202827] justify-center rounded-lg p-2 text-sm text-gray hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden'>
+className='inline-flex- h-10 w-10 items-center bg-[#white] justify-center rounded-lg p-2 text-sm text-gray hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-black-200 md:hidden'>
   <span className='sr-only'>Menu</span>
 </button>
 <div className='hidden w-full md:block md:w-auto' id="navbar-default">
  <ul className='mt-4 flex flex-col rounded-lg border border-gray-100 bg-[#1AB7FF]-50 p-4 font-medium md:mt:0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse'>
-    <li>
+    <li className="bg-[#267FF3]">
       <a href='#home'>Home</a>
     </li>
-    <li>
-      <a href='/about'>About</a>
+    <li className="bg-[#267FF3]">
+      <a   href='/about'>About</a>
     </li>
-    <li>
-      <a href='#projects'>Projects</a>
+    <li className="bg-[#267FF3]">
+      <a  href='#projects'>Projects</a>
     </li>
-    <li>
-      <a href='/contact'>Contact</a>
+    <li className="bg-[#267FF3]">
+      <a  href='/contact'>Contact</a>
     </li>
   </ul>
   </div>
@@ -40,31 +39,42 @@ className='inline-flex- h-10 w-10 items-center bg-[#202827] justify-center round
 </nav>
 
 
+
+
 {/* Hero Section */}
-<section className=" flex hero bg-[#202827] justify-center items-center h-screen">
-      <div className="flex-col px-6  mx-auto">
-        <h1 className='px-5 font-bold text-5xl'>Hello, I'm Elijah-Christian</h1>
-        <p className='px-5 font-bold text-3xl'>A passionate developer</p>
-        <Avatar className='mx-auto justify-center items-center mt-10'alt='Elijah-Christian Wiegand' src={propic} sx={{ width: 400, height: 400 }} /> 
-        {/* 3d fun here?  */}
-      </div>
-    </section>
-  
-    {/* About Section */}
-    <section id="about" className="h-[600px] mt-12 flex flex-col">
-        <h2 className="mx-auto text-2xl justify-center items-center font-bold">About Me</h2>
-        <p className=' flex-col mt-5 font-league-spartan font-xl text-pretty leading-7'> I am a Front- End Developer, from NY! 
-      I am just a tech geek, who has some experience in the field of web development. 
-      I bring a refreshing perspective to projects that epitomizes satisfied clients 
-      and the Organization. Projects and people develop overtime, this portfolio is a living document 
-      please check back to see anything new that I am working on! </p>
-     
-    </section>
+<section className="flex hero bg-[#161616] justify-center items-center h-screen relative flex-col md:flex-row">
+  <div className="flex flex-col md:absolute md:left-16 md:top-40 lg:left-24 lg:top-48 space-y-4 md:space-y-0 md:space-x-4">
+    <div className="text-white text-4xl md:text-6xl lg:text-8xl font-normal font-['League Spartan']">
+      Hi, I’m
+    </div>
+    <div className="text-white text-3xl md:text-5xl lg:text-7xl font-normal font-['League Spartan']">
+      Elijah-Christian
+    </div>
+    <div className="text-white text-xl md:text-3xl lg:text-5xl lg:mt-12 font-normal font-['League Spartan']">
+      A Passionate Software Developer
+    </div>
+  </div>
+  <div className="absolute right-8 bottom-8 md:right-16 md:bottom-16">
+    <Avatar className='mx-auto justify-center place-self-end' alt='Elijah-Christian Wiegand' src={propic} sx={{ width: 400, height: 400 }} /> 
+  </div>
+</section>
+
+{/* About Section */}
+<section id="about" className="h-auto mt-12 flex flex-col p-8 w-full bg-[#f0f4f8]">
+  <h2 className="mx-auto text-2xl justify-center items-center font-bold">About Me</h2>
+  <p className='flex-col mt-5 font-league-spartan text-xl text-pretty leading-7 px-5'>
+    I am a Front-End Developer, from NY! I am just a tech geek, who has some experience in the field of web development. 
+    I bring a refreshing perspective to projects that epitomizes satisfied clients and the Organization. 
+    I am a believer, that sometimes "Haste is Waste" and sometimes Less is More. 
+    Projects and people develop overtime, this portfolio is a living document please check back to see anything new that I am working on!
+  </p>
+</section>
+
 
     {/* Projects Section */}
-    <section id="projects" className="projects mt-8 h-full w-screen bg-[#202827] flex flex-col items-center">
+    <section id="projects" className="projects mt-8 h-auto w-screen bg-[#161616] flex flex-col items-center">
         <h2 className="text-4xl font-bold leading-10 text-center text-[#1AB7FF]">Here are some of my projects:</h2>
-        <div className=" inline-flex justify-center space-x-4">
+        <div className=" inline-flex justify-center space-x-4 bg-[#161616] ">
           <ProjectCard title="Liverpool FC" description="A project to show my love for the Reds! Click the image to see more!" image={Liverpool} link="https://psychedelic-observation-production.up.railway.app/" />
           <ProjectCard title="Group Chat Name Generator" description="Group Chat generator with a fun lottie file" image={groupChatGen} link="https://660f580d21f957209c188d87--groupchatnamegenerator.netlify.app/" />
           <ProjectCard title="Refreshed News" description="A new Refreshing feel to a News website" image={refreshedNews} link="https://www.loom.com/share/2a91706e4ca141858de41a3bf2a612ba" />
@@ -72,8 +82,12 @@ className='inline-flex- h-10 w-10 items-center bg-[#202827] justify-center round
       </section>
 
     {/* Contact Section */}
-   <ContactForm />  
-    <p>Made with love</p><p>Made with love</p>
+    <section id="contact" className="contact h-auto w-screen bg-[#161616] flex flex-col items-center">
+  
+  <div className="w-full max-w-2xl">
+    <ContactForm />
+  </div>
+</section>
   </div>
 
  
--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 import emailjs from 'emailjs-com';
 import { Link } from 'react-router-dom';
+import {EnvelopeIcon} from '@heroicons/react/20/solid'; 
 
 const ContactForm: React.FC = () => {
   const [name, setName] = useState('');
@@ -23,29 +24,30 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    <section id="contact" className="mt-12 contact py-20 bg-[#1AB7FF]-100 flex-col justify-center items-center flex">
-      <div className="bg-[#1AB7FF] h-[300px] rounded-lg w-full max-w-md mx-auto px-5">
-        <h2 className='text-4xl font-bold mb-8 text-center text-white'>Contact</h2>
-        <form className='space-y-6' ref={form} onSubmit={sendEmail}>
-          <ul>
-            <li>
-              <label htmlFor="name">Name</label>
-              <input onChange={e => setName(e.target.value)} className='bg-[#212121] mt-1 block w-full text-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' type="text" id="name" name="name" />
-            </li>
-            <li>
-              <label htmlFor="email">Email</label>
-              <input onChange={e => setEmail(e.target.value)} className='bg-[#212121] mt-1 block w-full text-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' type="email" id="email" name="email" />
-            </li>
-            <li>
-              <label htmlFor="message">Message</label>
-              <textarea onChange={e => setMessage(e.target.value)} className='bg-[#212121] mt-1 block w-full text-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' id="message" name="message"></textarea>
-            </li>
-        
-          </ul>
-          <button className='bg-[#2CF000] mx-auto my-auto w-full h-3/4 rounded-md hover:shadow-lg hover:shadow-[#1AB7FF] ' type="submit">Send</button>
-        </form>
-      </div>
-    </section>
+    <section id="contact" className="mt-12 contact py-20 bg-[#161616] flex-col justify-center items-center flex">
+    <div className="bg-[#1AB7FF] rounded-lg w-full max-w-md mx-auto px-5">
+      <h2 className='text-4xl font-bold mb-8 text-center text-white'>
+       <EnvelopeIcon className='h-8 w-8 inline-block text-white' /> Contact Me  
+      </h2>
+      <form className='space-y-6 w-full max-w-2xl mx-auto' ref={form} onSubmit={sendEmail}>
+  <ul className='space-y-4'>
+    <li>
+      <label htmlFor="name" className='block text-white'>Name</label>
+      <input onChange={e => setName(e.target.value)} className='bg-[#212121] mt-1 block w-full text-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' type="text" id="name" name="name" />
+    </li>
+    <li>
+      <label htmlFor="email" className='block text-white'>Email</label>
+      <input onChange={e => setEmail(e.target.value)} className='bg-[#212121] mt-1 block w-full text-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' type="email" id="email" name="email" />
+    </li>
+    <li>
+      <label htmlFor="message" className='block text-white'>Message</label>
+      <textarea onChange={e => setMessage(e.target.value)} className='bg-[#212121] mt-1 block w-full text-white border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' id="message" name="message" rows={4}></textarea>
+    </li>
+  </ul>
+  <button className='bg-[#2CF000] mx-auto my-auto w-full h-3/4 rounded-md hover:shadow-lg hover:shadow-[#1AB7FF]' type="submit">Send</button>
+</form>
+    </div>
+  </section>
   );
 };
 
