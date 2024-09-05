1830bb2ed6fd7c97494d9bf483ba3436ef45399f
EC Wiegand <ewiegand51@gmail.com>
2024-09-05 18:35:01 -0500

    small fixes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import groupChatGen from '../../public/imgs/GroupChatNameGen.png';
 import Liverpool from '../../public/imgs/Liverpoolshield.jpg';
 import ProjectCard from './projectCard';  
 import ContactForm from './contactForm';
-import Typical from 'react-typical';  
+
 // palette colors 1AB7FF -blue bright chartruese - 2CF000 202827
 const Home = () => (
   <div className="Home  bg-[#f0f4f8]">
@@ -20,18 +20,18 @@ className='inline-flex- h-10 w-10 items-center bg-[#white] justify-center rounde
   <span className='sr-only'>Menu</span>
 </button>
 <div className='hidden w-full md:block md:w-auto' id="navbar-default">
- <ul className='mt-4 flex flex-col rounded-lg border border-gray-100 bg-[#1AB7FF]-50 p-4 font-medium md:mt:0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse'>
+ <ul className='mt-4 flex flex-col rounded-lg border border-red-500 bg-[#1AB7FF]-50 p-4 font-medium md:mt:0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse'>
     <li className="bg-[#267FF3]">
-      <a href='#home'>Home</a>
+      <a href='#home' className='text-white'>Home</a>
     </li>
     <li className="bg-[#267FF3]">
-      <a   href='#about'>About</a>
+      <a className='text-white'  href='#about'>About</a>
     </li>
     <li className="bg-[#267FF3]">
-      <a  href='#projects'>Projects</a>
+      <a className='text-white' href='#projects'>Projects</a>
     </li>
     <li className="bg-[#267FF3]">
-      <a  href='#contact'>Contact</a>
+      <a className='text-white' href='#contact'>Contact</a>
     </li>
   </ul>
   </div>
@@ -42,19 +42,19 @@ className='inline-flex- h-10 w-10 items-center bg-[#white] justify-center rounde
 
 
 {/* Hero Section */}
-<section className="flex hero bg-[#161616] justify-center items-center h-screen relative flex-col md:flex-row">
-  <div className="flex flex-col md:absolute md:left-16 md:top-40 lg:left-24 lg:top-48 space-y-4 md:space-y-0 md:space-x-4">
+<section className="flex flex-col md:flex-row bg-[#161616] justify-center items-center h-screen relative">
+  <div className="order-2 md:order-1 flex flex-col space-y-4 md:space-y-0 md:space-x-4 md:relative md:left-16 md:top-40 lg:left-24 lg:top-48 lg:space-y-8">
     <div className="text-white text-4xl md:text-6xl lg:text-8xl font-normal font-['League Spartan']">
       Hi, I’m
     </div>
     <div className="text-white text-3xl md:text-5xl lg:text-7xl font-normal font-['League Spartan']">
       Elijah-Christian
     </div>
-    <div className="text-white text-xl md:text-3xl lg:text-5xl lg:mt-12 font-normal font-['League Spartan']">
+    <div className="text-white text-xl md:text-3xl lg:text-5xl font-normal font-['League Spartan']">
       A Passionate Software Developer
     </div>
   </div>
-  <div className="absolute right-8 bottom-8 md:right-16 md:bottom-16">
+  <div className="order-1 md:order-2 mt-8 md:mt-0 lg:ml-16">
     <Avatar className='mx-auto justify-center place-self-end' alt='Elijah-Christian Wiegand' src={propic} sx={{ width: 400, height: 400 }} /> 
   </div>
 </section>
@@ -62,12 +62,27 @@ className='inline-flex- h-10 w-10 items-center bg-[#white] justify-center rounde
 {/* About Section */}
 <section id="about" className="h-auto mt-12 flex flex-col p-8 w-full bg-[#f0f4f8]">
   <h2 className="mx-auto text-2xl justify-center items-center font-bold">About Me</h2>
+
   <p className='flex-col mt-5 font-league-spartan text-xl text-pretty leading-7 px-5'>
-    I am a Front-End Developer, from NY! I am just a tech geek, who has some experience in the field of web development. 
+    I am a Full-Stack Developer, from NY! I am just a tech geek, who has some experience in the field of web development. 
     I bring a refreshing perspective to projects that epitomizes satisfied clients and the Organization. 
     I am a believer, that sometimes "Haste is Waste" and sometimes Less is More. 
     Projects and people develop overtime, this portfolio is a living document please check back to see anything new that I am working on!
   </p>
+  <div className="flex flex-col mt-5 font-league-spartan text-xl text-pretty leading-7 px-5">
+    <p>Here are some of the technologies I have experience with:</p>
+    <ul className="list-disc list-inside">
+      <li>React Frameworks(React, Vite, NextJs)</li>
+      <li>PostgreSQL</li>
+      <li>JavaScript</li>
+      <li>Microsoft SQL Server Management Studio (SSMS)</li>
+      <li>Node.js</li>
+      <li>Express</li>
+      <li>PostgreSQL</li>
+      <li>Git</li>
+      <li>.NET</li>
+    </ul> 
+    </div>
 </section>
 
 
